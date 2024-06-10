c83d89175c2f7bcea1d97a3d6c0b8369380a794c
2nonnon <lanpangzi0205@gmail.com>
2024-06-10 22:18:02 +0800

    feat: init

---
+++ src/components/WindowTitlebar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { appWindow } from '@tauri-apps/api/window'
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+  <div data-tauri-drag-region class="titlebar">
+    <Button variant="outline" size="icon" @click="appWindow.minimize()">
+      <Icon icon="mdi:window-minimize" />
+    </Button>
+    <Button variant="outline" size="icon" @click="appWindow.toggleMaximize()">
+      <Icon icon="mdi:window-maximize" />
+    </Button>
+    <Button variant="outline" size="icon" @click="appWindow.close()">
+      <Icon icon="mdi:close" />
+    </Button>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,69 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import VueMacros from 'unplugin-vue-macros/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    VueMacros({
+      defineOptions: false,
+      defineModels: false,
+      plugins: {
+        vue: Vue({
+          script: {
+            propsDestructure: true,
+            defineModel: true,
+          },
+        }),
+      },
+    }),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        '@vueuse/core',
+        // 'vue-router',
+        {
+          // add any other imports you were relying on
+        },
+      ],
+      dts: './src/auto-imports.d.ts',
+      dirs: [
+        './src/composables',
+      ],
+      vueTemplate: true,
+    }),
+
+    // https://github.com/antfu/vite-plugin-components
+    Components({
+      dirs: ['src/components'],
+      dts: './src/components.d.ts',
+    }),
+  ],
+  build: {
+    target: 'esnext',
+  },
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+}))
