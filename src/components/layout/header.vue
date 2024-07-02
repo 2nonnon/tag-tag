99bf85323abfda3cceddd339ba01b4917136eb7c
2nonnon <lanpangzi0205@gmail.com>
2024-07-02 23:56:47 +0800

    打点修复

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div data-tauri-drag-region class="flex justify-between items-center px-8 py-4">
+  <div data-tauri-drag-region class="flex justify-between items-center px-8 py-3">
     <div class="flex items-center">
       <n-el class="w-[100px] h-[40px] text-[--primary-color]">
         <Logo class="w-full h-full" />
